620b8edd663ece517aea9f6122c868785c8c8162
pranavkhapra <pranavkhapra23@gmail.com>
2021-02-24 14:55:04 +0530

    added or joined the storepicker with the dashboard of the related name page and fixed some errors/completed the storepicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import {getFunName} from '../helpers'
 class StorePicker extends React.Component{
+    constructor(){
+        super()
+        this.goToStore=this.goToStore.bind(this)
+    }
+    myInput=React.createRef()
+    //the important thing is we have acced to router as we are its children and also you can see this in the react dev tools
+    goToStore(event){
+        //--things we have to achieve
+        //1.stop the form to automatic refreshing 
+        //2.get the text we are getting in the form
+        //3.Also change the page from StorePicker to the store page we are going to
+        event.preventDefault()
+        console.log(this.myInput.current.value)
+        const storeName=this.myInput.current.value
+        this.props.history.push(`/store/${storeName}`)
+
+    }
     render() {
+       
         return(
             <>
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore} >
             
             <h2>Please Enter A Store</h2>
-            <input type="text" placeholder="Store Name" defaultValue={getFunName()}></input>
+            <input 
+            ref={this.myInput}
+            type="text" placeholder="Store Name" defaultValue={getFunName()}></input>
             <button type="submit">Visit Store </button>
         </form>
         </>
