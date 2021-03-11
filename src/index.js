5bd3ff4bfa47a3e881bc012614ccb164b7b6f225
pranavkhapra <pranavkhapra23@gmail.com>
2021-03-11 19:37:28 +0530

    added some code to solve a css error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './components/Router'
-import "./css/style.css"
+import React from "react";
+import { render } from "react-dom";
+import Router from "./components/Router";
+import "./css/style.css";
 
-
-ReactDOM.render(<Router/>,document.getElementById('root'));
+render(<Router />, document.querySelector("#main"));
